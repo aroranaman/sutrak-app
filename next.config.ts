5c33a179d1a0c9002362a742e259760d4747fd3c
Naman Arora <namanarora.9866@gmail.com>
2025-10-26 15:22:45 +0000

    INTERNAL SERVER ERROR, CAN YOU CORRECT IT

--- next.config.ts
+++ next.config.ts
@@ -35,14 +35,6 @@ const nextConfig: NextConfig = {
     position: 'bottom-right',
   },
   allowedDevOrigins: ['**/*.cloudworkstations.dev'],
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      react: require.resolve('react'),
-      'react-dom': require.resolve('react-dom'),
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
