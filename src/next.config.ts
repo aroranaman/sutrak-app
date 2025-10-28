0a7d23756a8f20668f06528941fa1d4857411a9f
Naman Arora <namanarora.9866@gmail.com>
2025-10-28 01:00:04 +0000

    Make Webpack use your single React copy
    Edit next.config.ts (or next.co

---
+++ src/next.config.ts
@@ -0,0 +1,48 @@
+import type { NextConfig } from "next";
+import path from "path";
+
+const nextConfig: NextConfig = {
+  transpilePackages: ["@react-three/fiber", "@react-three/drei", "three"],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  devIndicators: {
+    position: 'bottom-right',
+  },
+  allowedDevOrigins: ['**/*.cloudworkstations.dev'],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      react: path.resolve(__dirname, "node_modules/react"),
+      "react-dom": path.resolve(__dirname, "node_modules/react-dom"),
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
